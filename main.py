26f26483bde8a1cec83a52e766bf0d166c077e76
Ivaylo Nenovski <inenovski95@gmail.com>
2020-08-21 17:23:19 +0300

    Added the ability to load boards from json files. Added function to draw Sudoku's borders

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 import sys
 import enum
+import json
 import pygame
 import pygame.freetype
 
@@ -18,27 +19,22 @@ class Color(enum.Enum):
     Grid = (96, 124, 139)
 
 
-class SudokuGame():
-    """Class that implements the logic of solving Sudoku by using
-    the Backtracking algorithm
-    """
-    #TODO: Implement the SudokuGame class
-    def __init__(self, how='random'):
-        pass
-        # self._grid
-
-
 class Cell():
     """Implementation of a single Sudoku cell. Use this class to keep track
     of a cell's color, coordinate positions, and current solution
     """
-    def __init__(self, row, col, width, height):
+    def __init__(self, row, col, width, height, number):
         self._row = row
         self._col = col
         self._xpos = row * width
         self._ypos = col * height
-        self._solution = float("inf")
-        self._color = Color.Unsolved
+
+        if number:
+            self._solution = number
+            self._color = Color.Solved
+        else:
+            self._solution = float("inf")
+            self._color = Color.Unsolved
 
     @property
     def solution(self):
@@ -57,6 +53,69 @@ class Cell():
         self._color = new_color
 
 
+class SudokuGame():
+    """Class that implements the logic of solving Sudoku by using
+    the Backtracking algorithm
+    """
+    #TODO: Implement the SudokuGame class
+    def __init__(self, path, cell_width, cell_height):
+        self._grid = self.init_grid(path)
+        self._cell_width = cell_width
+        self._cell_height = cell_height
+
+    def init_grid(self, path):
+        """Initialize a grid from a JSON file
+
+        Args:
+            path (str): Path to a JSON file containing the grid
+
+        Returns:
+            list: List of lists representing the current Sudoku game
+        """
+        grid = list()
+
+        with open(path) as f:
+            board_data = json.load(f)
+
+        for row in range(9):
+            grid.append(list())
+            for col, number in enumerate(board_data["board"]["row" + str(row + 1)]):
+                grid[row].append(Cell(row, col, self._cell_width, self._cell_height, number))
+
+        return grid
+
+
+def draw_grid_borders(screen, rows, cols, width, height):
+    """Draws the borders of the Sudoku game
+
+    Args:
+        screen (pygame.display): The surface to draw the grid on
+        rows (int): Sudoku rows
+        cols (int): Sudoku cols
+        width (int): Width of a single Sudoku cell
+        height (int): Height of a single Sudoku cell
+
+    Returns:
+        list: All points that need to be redrawn by pygame due to a change.
+              This way one saves resources and does not redraw the entire screen.
+    """
+    updated_points = list()
+
+    for row in range(rows):
+        line_width = 5 if row % 3 == 0 else 1
+
+        dims1 = pygame.draw.line(screen, Color.Grid.value, (0, row * height), (width * rows, height * row), width=line_width)
+        updated_points.append(dims1)
+
+        for col in range(cols):
+            line_width = 5 if col % 3 == 0 else 1
+
+            dims2 = pygame.draw.line(screen, Color.Grid.value, (col * width, row * height), (col * width, row * height + height), width=line_width)
+            updated_points.append(dims2)
+
+    return updated_points
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((810, 810))
@@ -66,7 +125,7 @@ def main():
     game_font = pygame.freetype.Font("./fonts/Fira Code Bold Nerd Font Complete.ttf", font_size)
     
     # The mid point of a single cell = (810 / 9) / 2. Use this to center the text when drawing
-    mid_point = 45
+    mid_point = 30
 
     while True:
         screen.fill(Color.Background.value)
@@ -77,6 +136,8 @@ def main():
             for k in range(1, 9):
                 text_surface, rect = game_font.render(str(i + 1), Color.Text.value)
                 screen.blit(text_surface, pygame.Rect(i*90 + mid_point, k*90 + mid_point, 90, 90))
+
+        draw_grid_borders(screen, 9, 9, 90, 90)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
